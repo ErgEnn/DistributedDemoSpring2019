499d0e9117d6fdca20d068311602f59cc6a681aa
Andres Käver <akaver@akaver.com>
2019-02-22 13:27:57 +0200

    cleanup

--- ContactSolution/WebApp/Startup.cs
+++ ContactSolution/WebApp/Startup.cs
@@ -87,6 +87,12 @@ namespace WebApp
 
             app.UseMvc(routes =>
             {
+
+                routes.MapRoute(
+                    name: "area",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
