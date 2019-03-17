d102f425fd104305f2f624ac56f3e1609ed618ce
Andres Käver <akaver@akaver.com>
2019-03-17 21:05:05 +0200

    .net core updated

--- ContactSolution/WebApp/Controllers/HomeController.cs
+++ ContactSolution/WebApp/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Xml;
 using WebApp.ViewModels;
 
 namespace WebApp.Controllers
