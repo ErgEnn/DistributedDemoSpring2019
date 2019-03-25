ed10e3a159e814f7f32c0c34bd00c505849c7c9f
Andres Käver <akaver@akaver.com>
2019-03-25 16:31:53 +0200

    dto

---
+++ ContactSolution/DTO/ContactTypeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DTO
+{
+    public class ContactTypeDTO
+    {
+        public int Id { get; set; }
+        public string ContactTypeValue { get; set; }
+
+        public int ContactCount { get; set; }
+    }
+}
