eb5d92b2454c742a7eac8881632faa9ae15f359b
Andres Käver <akaver@akaver.com>
2019-04-28 23:34:59 +0300

    Resource files, translations

--- ContactSolution/BLL.App.DTO/Person.cs
+++ ContactSolution/BLL.App.DTO/Person.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Versioning;
 using DAL.App.DTO.Identity;
 
 namespace BLL.App.DTO
@@ -9,14 +10,16 @@ namespace BLL.App.DTO
 
         public int Id { get; set; }
         
-        [MaxLength(64)]
-        [MinLength(1)]
-        [Required]
+        [MaxLength(64,ErrorMessageResourceName = "ErrorMessageMaxLength" ,ErrorMessageResourceType = typeof(Resources.Domain.Common))]
+        [MinLength(2, ErrorMessageResourceName = "ErrorMessageMinLength", ErrorMessageResourceType = typeof(Resources.Domain.Common))]
+        [Required(ErrorMessageResourceName = "ErrorMessageRequired", ErrorMessageResourceType = typeof(Resources.Domain.Common))]
+        [Display(Name = nameof(FirstName), ResourceType = typeof(Resources.Domain.Person))]
         public string FirstName { get; set; }
 
-        [MaxLength(64)]
-        [MinLength(1)]
-        [Required]
+        [MaxLength(64, ErrorMessageResourceName = "ErrorMessageMaxLength", ErrorMessageResourceType = typeof(Resources.Domain.Common))]
+        [MinLength(2, ErrorMessageResourceName = "ErrorMessageMinLength", ErrorMessageResourceType = typeof(Resources.Domain.Common))]
+        [Required(ErrorMessageResourceName = "ErrorMessageRequired", ErrorMessageResourceType = typeof(Resources.Domain.Common))]
+        [Display(Name = nameof(LastName), ResourceType = typeof(Resources.Domain.Person))]
         public string LastName { get; set; }
 
 
